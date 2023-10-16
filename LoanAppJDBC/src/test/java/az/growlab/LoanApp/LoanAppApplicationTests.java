d5bd236d54ff05d158efa43209aaf483e33590a6
Umid <vvumid@gmail.com>
2023-10-16 22:45:39 +0400

    changes

---
+++ LoanAppJDBC/src/test/java/az/growlab/LoanApp/LoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.growlab.LoanApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
