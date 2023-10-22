384243f3eb99e375da5d12465a155ef26dce7c0d
Umid <vvumid@gmail.com>
2023-10-22 20:38:56 +0400

    changes

--- ProblemSimulation/src/main/java/az/growlab/problemsimulation/repo/AuthorRepo.java
+++
@@ -1,7 +0,0 @@
-package az.growlab.problemsimulation.repo;
-
-import az.growlab.problemsimulation.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepo extends JpaRepository<Author, Integer> {
-}
